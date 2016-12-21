77ae8115e8ee796fa526db5afd499d19ec3baa53
Hanoman Putih <vanzilz18@gmail.com>
2016-12-21 13:35:50 +0700

    test commit

--- GarbageCollection/src/com/ivandhitya/garbagecollector/Explicit.java
+++ GarbageCollection/src/com/ivandhitya/garbagecollector/Explicit.java
@@ -52,6 +52,7 @@ class MainExplicit {
 
             //Must use either:
             System.gc();              //OR call Runtime.getRuntime().gc();
+            //test
 
             System.out.println("done");
         }
